45b0c6ab1c0b0ebb72ecf8e75080c6bcef64e3bf
carcarc <kochun1124@gmail.com>
2015-06-25 15:02:08 +0800

    Create Instantiate.cs
    Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.

---
+++ Instantiate.cs
@@ -0,0 +1,4 @@
+public GameObject object;                // The enemy prefab to be spawned.
+public Transform[] spawnPoints;         // An array of the spawn points this enemy can spawn from.
+
+Instantiate (object, spawnPoints[0].position, spawnPoints[0].rotation);
