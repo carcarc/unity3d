d5582308a490da8300438de0796fdd2305fe7909
carcarc <kochun1124@gmail.com>
2015-12-07 11:24:33 +0800

    Update and rename road_generation.cs to RoadGeneration.cs

---
+++ RoadGeneration.cs
@@ -0,0 +1,8 @@
+public class RoadGeneration : MonoBehaviour {
+ public GameObject Water;
+ public GameObject Road;
+ public GameObject Grass;
+
+ int firstRand;
+ int secondRand;
+ int distPlayer = 12;
