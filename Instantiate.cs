d35916be7e67bdf64dad0cc1ac962d295c0dba4a
carcarc <kochun1124@gmail.com>
2016-05-26 12:33:18 +0800

    Update Instantiate.cs

--- Instantiate.cs
+++ Instantiate.cs
@@ -1,3 +1,4 @@
+/*Duplicate an object*/
 public GameObject object;                // The enemy prefab to be spawned.
 public Transform[] spawnPoints;         // An array of the spawn points this enemy can spawn from.
 
