b393f24f10ea7c4c15383912e96d642abfb84596
carcarc <kochun1124@gmail.com>
2017-02-03 15:32:25 +0800

    Update FindWithTag.cs

--- FindWithTag.cs
+++ FindWithTag.cs
@@ -1,11 +1,11 @@
-private Generate gameGenerate;
+private AnyClass anyGameClass;
 
 void Start(){
   //FindWithTag
   GameObject gameControllerObject = GameObject.FindWithTag("GameController");
   if (gameControllerObject != null)
   {
-      gameGenerate = gameControllerObject.GetComponent<Generate>();
+      gameGenerate = gameControllerObject.GetComponent<AnyClass>();
   }
   if (gameGenerate == null)
   {
