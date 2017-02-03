58587855148a9b7b167db1b90820ddd12b4eecaf
carcarc <kochun1124@gmail.com>
2017-02-03 15:28:56 +0800

    Create FindWithTag.cs

---
+++ FindWithTag.cs
@@ -0,0 +1,10 @@
+//FindWithTag
+  GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+  if (gameControllerObject != null)
+  {
+      gameGenerate = gameControllerObject.GetComponent<Generate>();
+  }
+  if (gameGenerate == null)
+  {
+      Debug.Log("Cannot find 'GameController' script");
+  }
