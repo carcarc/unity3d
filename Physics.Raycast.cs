44d853beff375af77a75bca890610b946a7cffbf
carcarc <kochun1124@gmail.com>
2017-11-23 12:31:32 +0800

    Update Physics.Raycast.cs

--- Physics.Raycast.cs
+++ Physics.Raycast.cs
@@ -1,8 +1,8 @@
-RayCastHit hit;  //儲存物件
+RayCastHit hitany;  //儲存物件
 
 Ray myRay = new Ray(transform.position,Vector3.down); //射線方向
 
 //射線由myRay射出長度為DownHeight 碰到的物件為hit
-if(Physics.Raycast (myRay, out hit ,DownHeight) ){ 
+if(Physics.Raycast (myRay, out hitany ,DownHeight) ){ 
   //hit
 }
