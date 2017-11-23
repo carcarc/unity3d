811c9781aad5107b9d134b06e31342e6ee522fac
carcarc <kochun1124@gmail.com>
2017-11-23 11:51:15 +0800

    Update StartCoroutine.cs

--- StartCoroutine.cs
+++ StartCoroutine.cs
@@ -2,17 +2,16 @@ void Start () {
         Debug.Log("start1");
         StartCoroutine(Test());
         Debug.Log("start2");
-    }
+}
 
-    IEnumerator Test()
-    {
+IEnumerator Test(){
         Debug.Log("test1");
         yield return null;
         Debug.Log("test2");
-    }
-    
-    //Result
-    start1
-    test1
-    start2
-    test2
+}
+
+//Result
+start1
+test1
+start2
+test2
