14c3ed04a0f087e50504a38cf1784db383156ccd
carcarc <kochun1124@gmail.com>
2017-11-23 11:27:29 +0800

    Create StartCoroutine.cs

---
+++ StartCoroutine.cs
@@ -0,0 +1,18 @@
+void Start () {
+        Debug.Log("start1");
+        StartCoroutine(Test());
+        Debug.Log("start2");
+    }
+
+    IEnumerator Test()
+    {
+        Debug.Log("test1");
+        yield return null;
+        Debug.Log("test2");
+    }
+    
+    //Result
+    start1
+    test1
+    start2
+    test2
